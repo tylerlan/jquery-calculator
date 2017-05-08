d28f1ac3626e5a060129851f8b828c977abaace3
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-07 23:51:31 -0700

    Updated plan for the calculator, and added functions for evaluating imput and calculating and displaying output, including a simply error message

--- js/index.js
+++ js/index.js
@@ -1,88 +1,118 @@
-// let spans = $('span');
-// spans.click(console.log('SPAN!'));
-//
-//
-// let operators = $('.operator');
-// operators.click(function() {
-//   let text = this.text();
-//   console.log(text);
-// })
-//
+$( document ).ready(function() {
+
+  // let spans = $('span');
+  // spans.click(console.log('SPAN!'));
+  //
+  //
+  // let operators = $('.operator');
+  // operators.click(function() {
+  //   let text = this.text();
+  //   console.log(text);
+  // })
+  //
 
 
-/*
-When a span is clicked,
-1) push its content to the display array, which
-2) should be rendered immidiately in HTML
-*/
+  /*
+  When a span is clicked,
+  1) push its content to the display array, which
+  2) should be rendered immidiately in HTML
+  */
 
-let display = [];
+  let display = [];
 
-$('span').click(function() {
-  display.push(this.text());
-  renderDisplay();
-})
+  $('span').click(function() {
+    display.push(this.text());
+    renderDisplay();
+  })
 
-function renderDisplay() {
-  $('#screen').text(display.join(""));
-}
+  function renderDisplay() {
+    $('#screen').text(display.join(""));
+  }
 
-/*
-When a span with id of 'equals' is clicked, it should evaluate the display array using forEach()
+  /*
+  When a span with id of 'equals' is clicked, it should evaluate the display array using forEach()
 
-- Looping over the array, it should check if isNaN() each element that is passed through a parseInt()
+  - Looping over the array, it should check if isNaN() each element that is passed through a parseInt()
 
-- When it encounters something that is Not a Number, it should check which operator it is a run a specialized function(){}
+  - When it encounters something that is Not a Number, it should check which operator it is a run a specialized function, one each for every operator
+  */
 
-- Each function should split() the array, passing in the NaN element, and and save the new array to calculation.
-Then, it should break out of the loop and move on.
-*/
+  $('#equals').click(evaluate());
 
-$('#equals').click(evaluate());
+  function evaluate() {
+    if (display.length === 0) {
+      return error();
+    }
 
-function evaluate() {
-    display.forEach( (element) => {
-      if (isNaN(parseInt(element))) {
-        if (element === '÷') {
-          divide(element);
-        } else if (element === 'x') {
-          multiply(element);
-        } else if (element === '-') {
-          subtract(element);
-        } else if (element === '+') {
-          add(element);
+      display.forEach( (element) => {
+        if (isNaN(parseInt(element))) {
+          if (element === '÷') {
+            divide(element);
+          } else if (element === 'x') {
+            multiply(element);
+          } else if (element === '-') {
+            subtract(element);
+          } else if (element === '+') {
+            add(element);
+          }
         }
-      }
-    } )
+      } )
+  }
+
+
+  /*
+  Each operator function should split() the array, passing in the NaN element, change the string terms into numbers, and and save the new array to the variable terms.
+
+  The terms array should hold two elements (two mathematical terms) which are strings of numbers. Check that this is true, and if not, throw an error.
+
+  Calculation can be done simply by performing the operation of the first term (index 0) by the second term (index 1).
+
+  The result should be rendered to the screen.
+  */
+
+  let terms, result;
+
+  function divide(operator) {
+    terms = display.join("").split(operator).map(element => parseInt(element));
+    result = terms[0] / terms[1];
+    return $('#screen').text(result);
+  }
+
+  function multiply(operator) {
+    terms = display.join("").split(operator).map(element => parseInt(element));
+    result = terms[0] * terms[1];
+    return $('#screen').text(result);
 }
 
-let calculation = [];
-
-function divide(operator) {
-  calculation = display.join("").split(operator)
+  function subtract(operator) {
+    terms = display.join("").split(operator).map(element => parseInt(element));
+    result = terms[0] - terms[1];
+    return $('#screen').text(result);
 }
 
-function multiply(operator) {
-  calculation = display.join("").split(operator)
+  function add(operator) {
+    terms = display.join("").split(operator).map(element => parseInt(element));
+    result = terms[0] + terms[1];
+    return $('#screen').text(result);
+
 }
 
-function subtract(operator) {
-  calculation = display.join("").split(operator)
-}
+  /*
+  When a span with id of 'clear' is clicked, display array should be empitied and updated so the html rendering is clear as well
+  */
 
-function add(operator) {
-  calculation = display.join("").split(operator)
-}
+  $('#clear').click() {
+    display = [];
+    renderDisplay()
+  }
 
-/*
-The calculation array should hold two elements which are strings of numbers (check that this is true, and if not, throw an error)
-Calculation should be evaluated with reduce(), and run an operation based on the operator used. This result should be rendered to the screen.
-*/
 
-$('#screen').text()
+  /*
+  There should be an error function to call whenever something weird happens. When the error() function is called, it should print ERROR to the screen and to the console.
+  */
+  function error() {
+    console.log('ERROR');
+    $('#screen').text('ERROR');
+  }
 
-/*
-When a span with id of 'clear' is clicked, display array should be empitied and updated so the html rendering is clear as well
-*/
-
-$('#clear').click()
+});
